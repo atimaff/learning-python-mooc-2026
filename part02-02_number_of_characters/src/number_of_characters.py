61053e1be1a1d82fd5da7a4e3faa5c566ba4ddd9
Atilio Maffini <maffiniatilio@gmail.com>
2026-04-16 12:45:53 -0300

    Solo ejercicios realizados en vscode

--- part02-02_number_of_characters/src/number_of_characters.py
+++ part02-02_number_of_characters/src/number_of_characters.py
@@ -1,1 +1,10 @@
 # Write your solution here
+'''
+Please type in a word: hey
+There are 3 letters in the word hey
+Thank you!
+'''
+word=input('PLease type in a word : ')
+if len(word) > 1:
+    print(f"There are {len(word)} letters in the word {word}")
+print('Thank you!')
